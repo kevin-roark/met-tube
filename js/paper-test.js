4320dcb4f2af9e91744c3c1b9555cadaf9fdc213
bfasenfest <aowbackup@gmail.com>
2016-08-08 20:58:27 -0400

    attempt to make it scalable and work with rasters -- failed

--- js/paper-test.js
+++ js/paper-test.js
@@ -1,10 +1,10 @@
 // Config
 var imageURL = 'mona.jpg';
-var captureFactor = 7;
+var captureFactor = 30;
 var captureWidth = view.size.width / captureFactor;
 var captureHeight = view.size.height / captureFactor;
 var previewFactor = 4;
-var speed = 100; // higher is slower (upside down smiley face)
+var speed = 20; // higher is slower (upside down smiley face)
 var skip = 0;
 
 // State
@@ -30,9 +30,15 @@ raster.onLoad = function () {
 
     loaded = true;
 
-    // var rect2 = new Rectangle(500, 200, length, length);
+    // var rect2 = new Rectangle(0, view.size.height, captureWidth, captureHeight);
+    // var path2 = new Path.Rectangle(rect2);
+    // path2.strokeColor = 'red';
+    // var path3 = path2.clone();
     // var sub = raster.getSubRaster(rect2);
-    // sub.fitBounds(view.bounds, true);
+    // sub.position = view.center;
+    //
+    // path3.position = view.center;
+    // path3.strokeWidth = 5;
 
 };
 
@@ -52,19 +58,22 @@ var outlineRect = new Path.Rectangle({
 });
 
 
-
 function onFrame (event) {
     if (!loaded) return;
 
     if (event.count % speed === 0) {
-        var colorIndex = ((event.count * (skip + 1)) / speed) % subRasters.length;
-        //displayRect.fillColor = rasterColors[colorIndex];
-        var previousIndex = colorIndex > 0 ? colorIndex - 1 : subRasters.length - 1;
-        subRasters[previousIndex].visible = false;
+        var colorIndex = (((event.count * (skip + 1)) / speed)) % rasterColors.length;
+        displayRect.fillColor = rasterColors[colorIndex];
 
-        subRasters[colorIndex].fitBounds(view.bounds);
-        subRasters[colorIndex].sendToBack();
-        subRasters[colorIndex].visible = true;
+        console.log(colorIndex);
+
+        // var previousIndex = colorIndex > 0 ? colorIndex - 1 : subRasters.length - 1;
+        // subRasters[previousIndex].visible = false;
+        //
+        //
+        // subRasters[colorIndex].fitBounds(view.bounds);
+        // subRasters[colorIndex].sendToBack();
+        // subRasters[colorIndex].visible = true;
 
         outlineRect.position.x += captureWidth / previewFactor;
         if (outlineRect.position.x >= (view.size.width) / previewFactor){
@@ -80,22 +89,21 @@ function onFrame (event) {
     }
 }
 
-console.log(outlineRect);
 
 function getColors (r) {
     // Fill colors with sequential color-squares
     var colors = [];
     var subRasterArray = [];
-    for (var y = 0; y < view.size.height - captureHeight; y += captureHeight) {
-        for (var x = 0; x < view.size.width - captureWidth; x += captureWidth) {
+    for (var y = 0; y < view.size.height ; y += captureHeight) {
+        for (var x = 0; x < view.size.width; x += captureWidth) {
             var rect = new Rectangle(x, y, captureWidth, captureHeight);
             var path = new Path.Rectangle(rect);
             var color = r.getAverageColor(path);
             colors.push(color);
 
-            var subRasterTemp = r.getSubRaster(rect);
-            subRasterTemp.visible = false;
-            subRasterArray.push(subRasterTemp);
+            //var subRasterTemp = r.getSubRaster(rect);
+            //subRasterTemp.visible = false;
+            //subRasterArray.push(subRasterTemp);
 
         }
     }
